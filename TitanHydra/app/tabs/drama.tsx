8deb8494a197fc6e7f0d404ff7f31b5cef081893
LucasChambi <lucasbrunocc880@outlook.com>
2025-08-25 20:26:51 -0300

    Filmes de Drama Adicionado

--- TitanHydra/app/tabs/drama.tsx
+++ TitanHydra/app/tabs/drama.tsx
@@ -12,6 +12,39 @@ import {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   Image,
   ScrollView,
   StatusBar,
@@ -37,23 +70,23 @@ const MovieScreen = () => {
     { id: '101', title: '7 prisioneiros', image: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTWXJZF1-IL8SL7ASz3l4J5jHx6jcjXy0oTomc8HoEy2E4q1cXX' },
     { id: '102', title: 'Historia de um casamento', image: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcT-ulgxB7Zm7IwKO-xjTcDLvFJQaYp0wG-A0U8VreLpJjRejqyd' },
     { id: '103', title: 'A vida é invisivel', image: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSBgLRsv6l13oka7uP1Q6Lh4GFuHgRArdZikYckLS1UZDU5fFBK' },
-    { id: '104', title: 'O ultimo paraíso', image: 'hhttps://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRsxlcId30vIWwJUFKl1TCpm5O38yotiwmKVsMtBDAd65TB6cyo' },
-    { id: '105', title: 'Bob Esponja: O filme', image: 'https://upload.wikimedia.org/wikipedia/pt/2/2f/6875.jpg' },
-    { id: '106', title: 'Monstros S.A', image: 'https://upload.wikimedia.org/wikipedia/pt/7/75/Monsters_Inc.jpg' },
-    { id: '107', title: 'Carros', image: 'https://upload.wikimedia.org/wikipedia/pt/9/9b/Carros_p%C3%B4ster.jpg' },
-    { id: '108', title: 'Garfield: Fora de Casa', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0F5H6gdg59kKwY0XkPNu3Ipd4-HqjSj77kw&s' },
-    { id: '109', title: 'Meu Malvado Favorito 2', image: 'https://upload.wikimedia.org/wikipedia/pt/2/26/Despicable_Me_2.jpg' },
-    { id: '110', title: 'Os Caras Malvados 2', image: 'https://tse2.mm.bing.net/th/id/OIF.kUc6I94HAXdWY07RvxQPfQ?rs=1&pid=ImgDetMain&o=7&rm=3' },
-    { id: '111', title: 'Gato de Botas 2', image: 'https://www.atoupeira.com.br/wp-content/uploads/2023/01/gato-de-botas-2-poster-nacional-critica.jpg' },
-    { id: '112', title: 'Up: Altas Aventuras', image: 'https://upload.wikimedia.org/wikipedia/pt/a/a8/Up_p%C3%B4ster.jpg' },
-    { id: '113', title: 'Procurando Nemo', image: 'https://upload.wikimedia.org/wikipedia/pt/2/29/Finding_Nemo.jpg' },
-    { id: '114', title: 'Viva: A vida é uma Festa', image: 'https://upload.wikimedia.org/wikipedia/pt/a/ae/Cocofilme.png' },
-    { id: '115', title: 'Rango', image: 'https://m.media-amazon.com/images/M/MV5BMTc4NjEyODE1OV5BMl5BanBnXkFtZTcwMjYzNTkxNA@@._V1_.jpg' },
-    { id: '116', title: 'Wall-e', image: 'https://play-lh.googleusercontent.com/s90tRLDG8npS1xHbN3Hu7c7yBdLyv05CuLOwIjAR1Lsk8fj-jCiPiMkdbHamxLPQDTBQ' },
-    { id: '117', title: 'Os Incriveis', image: 'https://upload.wikimedia.org/wikipedia/pt/4/4b/Os_Incr%C3%ADveis.jpg' },
-    { id: '118', title: 'Angry Birds: O filme', image: 'https://upload.wikimedia.org/wikipedia/pt/e/e5/Angry_birds_poster.jpg' },
-    { id: '119', title: 'Meu Malvado Favorito 4', image: 'https://br.web.img3.acsta.net/img/6c/71/6c71afa89fd8ed8999b3e04d8d794a0e.jpg' },
-    { id: '120', title: 'Ratatouille', image: 'https://www.papodecinema.com.br/wp-content/uploads/2012/08/20181221-medium-ratatouille-poster-size-12-inch-x-18-inch-pack-of-1-original-imafy8d2aqshpguj.webp' },
+    { id: '104', title: 'O ultimo paraíso', image: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRsxlcId30vIWwJUFKl1TCpm5O38yotiwmKVsMtBDAd65TB6cyo' },
+    { id: '105', title: 'Como eu era antes de você', image: 'https://m.media-amazon.com/images/M/MV5BMjQ4ZGMyZjYtNDYwNC00MmUyLWE4ZWQtM2ViMTQ0OTc1M2U1XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg' },
+    { id: '106', title: 'A espera de um milagre', image: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTgfL8fCk0-J2UAiA87wBdW5_z66tbJuj6QZX-7fszBkwnuBAcd' },
+    { id: '107', title: 'Que horas ela volta?', image: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQvgtwwMd9geVvCir9mg0NQG3YWoM4cmxUJP-FJul508Ov70FSH' },
+    { id: '108', title: 'O segredo dos seus olhos', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQV_fup5agawL5_PC7QqIF0dx8rp0iW0H7gYd8JPzsbfOH90Pjp' },
+    { id: '109', title: 'O lobo atrás da porta', image: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQEs-mmvyrQhA-k4fc5xLPqE6UPCTUDTX93S2LNytY8zKIFC4PR' },
+    { id: '110', title: 'Inéxplicavel', image: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSocUp0DtVKlFUltjr7FXbg_ue45WXfuWeGNMyMIl_UQ4h3RekV' },
+    { id: '111', title: 'Sonhos roubados', image: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcS0GkIuWAW-F8BWuIjM188NGXHUXMovQ8DgkidJ2khGmMPZ1src' },
+    { id: '112', title: 'Taxi Driver - Motorista de Taxi', image: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQZm_Vrjc_j2EKMFkJe06rSXjyCibwDfdK4UgTcWv9_INX-E7o_' },
+    { id: '113', title: 'O caso de Cristo', image: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRZN7lC7eepF0vD8PkbU7-CDv9KYidIY2ZN2dpkoNbxf9g3byr5' },
+    { id: '114', title: 'O desconhecido', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQS0KlK7ko9PFJS-3a0AhnTVwZKKVut5AX5OOB2J23AWudc5U7_' },
+    { id: '115', title: 'Fome de Sucesso', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzrLEEEoqHEqYZVwcCDkH1VEAuZMMUvi6vLYG182unu9QJu1_S' },
+    { id: '116', title: '127 Horas', image: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRr7cRamJF_LpCMMbS8sw2flVWWNKSgTaayKp9MH-zvIAJowzh7' },
+    { id: '117', title: 'Meu nome é radio', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5slb2qgxwuLrAMZHoRzbc1ZDUxw3BJrWoi9uey21CcLcYPyCH' },
+    { id: '118', title: 'As nadadoras', image: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcS__r48C8XfRB_v6OvkoXwtrezt_FOEUoMZbJN0NBJ-zeCa5yBk' },
+    { id: '119', title: 'Nada a perder', image: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTprlPN9XtHUKjfIhsxy26eA5zpguBImR8wUpWcsg3v89efKL8l' },
+    { id: '120', title: 'Um sonho possivel', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmxHTLKIqSa6s-zvcNrmfrFpJXinYE3oeP-GT5pNpGFzQNquLz' },
   ];
 
   // Função para agrupar os filmes em linhas de 4 para o grid
